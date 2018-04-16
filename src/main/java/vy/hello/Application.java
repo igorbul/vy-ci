984c6c6c865aaf2efbfc80137e8fdaa43dce45e7
igorbul <igor.vykhodtsev@gmail.com>
2018-04-16 22:50:53 +0300

    First version

---
+++ src/main/java/vy/hello/Application.java
@@ -0,0 +1,15 @@
+package vy.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ig on 16.04.18.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
